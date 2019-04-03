8dc277ffc9f788c6e83c40cfff08dd99c7984daa
visualstudio <visualstudio@122-PC34>
2019-04-03 09:37:04 -0400

    Initial

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace ConsoleApp8
+namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Download();
+        }
+
+        static void Download()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+                Console.WriteLine("Complete");
+            });
         }
     }
 }
